21474f205df92ec055acfd6652ec7eab0f608a07
Legion1900 <rom4ik730@gmail.com>
2020-03-27 17:42:34 +0200

    Textile init is ready.

--- app/src/main/java/com/legion1900/textiletour/MainActivity.kt
+++ app/src/main/java/com/legion1900/textiletour/MainActivity.kt
@@ -2,11 +2,21 @@ package com.legion1900.textiletour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this)[MainActivityViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel
+//        peer_profile.text = viewModel.peer.toString()
     }
 }
---
+++ app/src/main/java/com/legion1900/textiletour/MainActivityViewModel.kt
@@ -0,0 +1,43 @@
+package com.legion1900.textiletour
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import io.textile.textile.Textile
+
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+    private val recoveryPhrase: String
+
+    private val prefs =
+        getApplication<Application>().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
+    init {
+        recoveryPhrase = initTextile()
+        Log.d("test", "Phrase: $recoveryPhrase")
+    }
+
+//    val peer = Textile.instance().profile.get()
+
+    private fun initTextile(): String {
+        val isDebugLogLvl = true
+        val isWriteToDisk = false
+        val phrase: String? = Textile.initialize(getApplication(), isDebugLogLvl, isWriteToDisk)
+        Log.d("test", "Textile init phrase: $phrase")
+        return phrase?.apply { savePhrase(this) } ?: loadPhrase()
+    }
+
+    private fun savePhrase(phrase: String) {
+        with(prefs.edit()) {
+            putString(KEY_PHRASE, phrase)
+            apply()
+        }
+    }
+
+    private fun loadPhrase() = prefs.getString(KEY_PHRASE, "")!!
+
+    private companion object {
+        const val PREF_NAME = "TEXTILE_DATA"
+        val KEY_PHRASE = "${MainActivityViewModel::class.java.simpleName}.RECOVERY_PHRASE"
+    }
+}
