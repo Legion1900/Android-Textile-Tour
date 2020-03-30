167fc27c067d8fe8502f96db59152a7ceb8decfa
Legion1900 <rom4ik730@gmail.com>
2020-03-30 16:49:40 +0300

    Base setup for search screen.

--- app/src/main/java/com/legion1900/textiletour/view/search/SearchActivity.kt
+++ app/src/main/java/com/legion1900/textiletour/view/search/SearchActivity.kt
@@ -1,13 +1,70 @@
 package com.legion1900.textiletour.view.search
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.legion1900.textiletour.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
+
+//    TODO: finish later
+    private val viewModel by lazy { ViewModelProviders.of(this)[SearchViewModel::class.java] }
+
+    private val SEARCH_BY_NAME = getString(R.string.search_by_name)
+    private val SEARCH_BY_ADDRESS = getString(R.string.search_by_address)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        observeViewModel()
+        initSpinner()
+    }
+
+    private fun observeViewModel() {
+        viewModel.searchBy.observe(this, Observer {
+            when (it) {
+                SEARCH_BY_NAME -> {
+                    search_by_name_view.visibility = View.VISIBLE
+                    search_by_address_view.visibility = View.GONE
+                }
+                SEARCH_BY_ADDRESS -> {
+                    search_by_name_view.visibility = View.GONE
+                    search_by_address_view.visibility = View.VISIBLE
+                }
+            }
+        })
+
+        viewModel.searchResult.observe(this, Observer {
+
+        })
+    }
+
+    private fun initSpinner() {
+        search_by_spinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.search_by,
+            android.R.layout.simple_spinner_item
+        ).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+
+        search_by_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) { /* Nothing to do here */ }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                viewModel.searchBy.value = search_by_spinner.adapter.getItem(position) as String
+            }
+        }
     }
 }
--- app/src/main/java/com/legion1900/textiletour/view/search/SearchViewModel.kt
+++ app/src/main/java/com/legion1900/textiletour/view/search/SearchViewModel.kt
@@ -1,18 +1,49 @@
 package com.legion1900.textiletour.view.search
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import com.legion1900.textiletour.view.base.BaseViewModel
+import io.textile.pb.Model
 import io.textile.pb.QueryOuterClass
+import io.textile.textile.BaseTextileEventListener
 import io.textile.textile.Textile
 
-class SearchViewModel : ViewModel() {
+class SearchViewModel : BaseViewModel() {
+
+    val searchBy = MutableLiveData<String>()
+
+    val searchResult = MutableLiveData<List<Model.Contact>>()
+
+    private val results = mutableListOf<Model.Contact>()
+
+    private val resultListener = object : BaseTextileEventListener() {
+        override fun contactQueryResult(queryId: String?, contact: Model.Contact?) {
+            results += contact!!
+        }
+
+        override fun queryDone(queryId: String?) {
+            searchResult.postValue(results)
+        }
+    }
+
+    init {
+        textile.addEventListener(resultListener)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        textile.removeEventListener(resultListener)
+    }
+
     fun searchContactByAddress(address: String) {
+        results.clear()
         val options = QueryOuterClass.QueryOptions.newBuilder().setWait(10).setLimit(1).build()
         val query = QueryOuterClass.ContactQuery.newBuilder().setAddress(address).build()
         Textile.instance().contacts.search(query, options)
     }
 
     fun searchContactByName(name: String) {
+        results.clear()
         val options = QueryOuterClass.QueryOptions.newBuilder()
             .setWait(10)
             .setLimit(1)
